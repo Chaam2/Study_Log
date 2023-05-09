48b11749b8438c3a3fb5f42799738a8f4bdd5151
cham2 <dearcham2@gmail.com>
2023-05-09 22:18:16 +0900

    리액트 카운터

--- Elice/react-ts/src/App.tsx
+++ Elice/react-ts/src/App.tsx
@@ -8,7 +8,7 @@ type CounterProps = {
 function Counter({ title, initValue = 0 }: CounterProps) {
   const [count, setCount] = useState<number>(initValue);
   function up() {
-    setCount(count + 1);
+    setCount(()=>count + 1);
   }
   return (
     <>
@@ -27,7 +27,6 @@ function getCurrentTime() {
 }
 function Counter2() {
   const [times, setTimes] = useState<string[]>([]);
-  console.log('component - times', times);
   function up() {
     // times.push(getCurrentTime());
     // setTimes(times);
@@ -40,16 +39,57 @@ function Counter2() {
       <h1>Counter2</h1>
       <button onClick={up}>+</button>
       <ol>
-        {<li>{times}</li>}
+        {times.map((v,i)=><li key={i}>{v}</li>)}
       </ol>
     </>
   );
 }
+function Counter3(){
+  const [count,setCount]=useState<number>()
+  const [step,setStep]=useState<number>(10)
+
+  return<>
+  <h1>Counter3</h1>
+  <input type='number' value={step} onChange={(e)=>setStep(Number(e.target.value))}></input>
+  <button onClick={()=>setCount((count===undefined?0:count)+step)}>+</button>
+  </>
+}
+
+type countType={
+  time:string;
+  step:number;
+}
+function Counter4() {
+  const [count,setCount]=useState<countType[]>([])
+  const[step,setStep]=useState(1)
+  let total = 0;
+for(let i=0; i<count.length; i++){
+    total = total + count[i].step;
+}
+
+  return (
+    <>
+      <h1>Counter4</h1>
+      <input type="number" value={step} onChange={(e)=>{setStep(Number(e.target.value))}}/>
+      <button onClick={()=>{
+        const newCount = [...count]
+        newCount.push({time:getCurrentTime(), step:step})
+        setCount(newCount)
+      }}>+</button>{total}
+      <ol>
+        {count.map((v,i)=><li key={i}>{v.time}</li>)}
+      </ol>
+    </>
+  );
+}
+
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={10}></Counter>
       <Counter2></Counter2>
+      <Counter3></Counter3>
+      <Counter4></Counter4>
     </div>
   );
 }
