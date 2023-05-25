720e55483de4d43976cb4cba5678e4e1b64cacb8
cham2 <dearcham2@gmail.com>
2023-05-25 13:47:08 +0900

    .env파일에 설정정보 담고 env.template을 만들어준다

--- Elice/next-practice/pages/read/[id].tsx
+++ Elice/next-practice/pages/read/[id].tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 export const getServerSideProps = async (context) => {
   console.log('getServerSideProps');
   const response = await fetch(
-    'http://localhost:3000/api/topics/' + context.query.id
+    process.env.API_URL + '/api/topics/' + context.query.id
   ); //풀 경로를 넣어줘야한다(context)는 서버에서 실행되기때문
   const result = await response.json();
   return {
@@ -24,6 +24,10 @@ export default function Read(props) {
       </Head>
       <h2>{props.title}</h2>
       {props.body}
+      {/* {process.env.API_URL}
+        //환경변수는 기본적으로 컴포넌트에선 가져올 수 없다
+        //클라이언트에서 가져오고 싶다면 NEXT_PUBLIC_API_URL로 접두사 설정할 것
+        */}
     </>
   );
 }
