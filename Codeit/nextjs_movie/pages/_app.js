f1324984370bdba2d656d3920064c6275c7b7f21
cham2 <dearcham2@gmail.com>
2023-07-11 19:05:11 +0900

    모든 페이지 공통 컴포넌트는 _app.js에서 설정, html 구조 수정은 _document.js(SSR시에만 실행되는 코드를 포함할 수도 있음)

--- Codeit/nextjs_movie/pages/_app.js
+++ Codeit/nextjs_movie/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css';
+import Headers from '@/components/Header';
+import Container from '@/components/Container';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Headers />
+      <Container page>
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
--- Codeit/nextjs_movie/pages/index.js
+++ Codeit/nextjs_movie/pages/index.js
@@ -1,8 +1,6 @@
 import MovieList from '@/components/MovieList';
 import SearchForm from '@/components/SearchForm';
 import styles from '@/styles/Home.module.css';
-import Header from '@/components/Header';
-import Container from '@/components/Container';
 import axios from '@/lib/axios';
 import { useEffect, useState } from 'react';
 
@@ -22,11 +20,8 @@ export default function Home() {
 
   return (
     <>
-      <Header />
-      <Container page>
-        <SearchForm />
-        <MovieList className={styles.movieList} movies={movies} />
-      </Container>
+      <SearchForm />
+      <MovieList className={styles.movieList} movies={movies} />
     </>
   );
 }
