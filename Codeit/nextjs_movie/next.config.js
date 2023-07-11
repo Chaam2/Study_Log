89229dbe6ed5f073de616757bbd60d52a6c8afff
cham2 <dearcham2@gmail.com>
2023-07-11 17:20:18 +0900

    next.config.js에서 리다이렉트 설정하기

--- Codeit/nextjs_movie/next.config.js
+++ Codeit/nextjs_movie/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/movies/:id',
+        destination: '/films/:id',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+/*
+redirect - https://nextjs.org/docs/app/api-reference/next-config-js/redirects
+source : 변경할 주소 / destination : 리다이렉트 할 주소
+permanent : true->브라우저가 리다티렉트를 저장(캐싱)함(308 status) / false->캐싱하지 않음(307 status)
+*/
